d924c12afb7e54037b67ef452ad28b18f85f49fa
Administrator <Administrator@172.31.18.242>
2016-12-10 14:27:45 +0100

    Little fix on list name

--- src/Run2.java
+++ src/Run2.java
@@ -1,7 +1,14 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,7 +18,7 @@ public class Run2 {
 
 	public static void main(String[] args) {
 		String lastData = "";
-		
+
 		while (true) {
 			try {
 				Document homeDoc = Jsoup
@@ -20,7 +27,8 @@ public class Run2 {
 								"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
 						.get();
 				Elements aElements = homeDoc.getElementsByClass("entry-title td-module-title").select("a[href]");
-				String data = aElements.get(0).attr("title").split(" ")[5];
+				int numTitleWords = aElements.get(0).attr("title").split(" ").length;
+				String data = aElements.get(0).attr("title").split(" ")[numTitleWords - 1];
 
 				if (!lastData.equals(data)) {
 					String dailyListLink = aElements.get(0).attr("href");
@@ -33,6 +41,7 @@ public class Run2 {
 					BufferedWriter bw = new BufferedWriter(new FileWriter(new File(title + ".m3u")));
 					bw.write(divElements.get(0).ownText());
 					bw.close();
+					uploadListToFtpServer();
 					lastData = data;
 				}
 			} catch (IOException e) {
@@ -44,6 +53,47 @@ public class Run2 {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private static void uploadListToFtpServer() throws IOException {
+		File dir = new File(System.getProperty("user.dir"));
+		File[] m3uLists = dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".m3u");
+			}
+		});
+		String ftpUrl = "ftp://%s:%s@%s/%s;type=i";
+		String host = "testprova.net23.net";
+		String user = "a4560590";
+		String pass = "missdepp90";
+
+		for (File list : m3uLists) {
+			String uploadPath = "public_html/bestrepo/PREMIUM/" + list.getName();
+
+			String formattedFtpUrl = String.format(ftpUrl, user, pass, host, uploadPath);
+			System.out.println(getTimestamp() + " Upload URL: " + formattedFtpUrl);
+
+			URL url = new URL(formattedFtpUrl);
+			URLConnection conn = url.openConnection();
+			OutputStream outputStream = conn.getOutputStream();
+			FileInputStream inputStream = new FileInputStream(list.getName());
+
+			byte[] buffer = new byte[4096];
+			int bytesRead = -1;
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
+
+			inputStream.close();
+			outputStream.close();
+
+			System.out.println(getTimestamp() + " File " + list.getName() + " uploaded");
+		}
+	}
+
+	private static String getTimestamp() {
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
+		return "[" + df.format(new Date()) + "]";
 	}
 
 }
